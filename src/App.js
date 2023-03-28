7b05ccd757958496775e198c85ae78aba0b0b752
Otmane Aatik <aaotmane@gmail.com>
2023-03-28 10:28:01 -0400

    initial settings scss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div className="App">
-      
+      Hello
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//sass
+import "./style/index.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
