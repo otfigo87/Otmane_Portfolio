5710aad9445e7ad36006f8a955fbec69ad6bcff0
Otmane Aatik <aaotmane@gmail.com>
2023-03-28 14:14:19 -0400

    social media component

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const SocialMedia = () => {
+
+  //onMouseOver animations
+    const anim = () => {
+        const icons = document.querySelectorAll(".social-network a");
+        // console.log(icons)
+        icons.forEach((link) => {
+            link.addEventListener("mouseover", (e) => {
+                link.style.transform = `translate(${e.offsetX - 20}px, ${
+                  e.offsetY - 13
+                }px)`;
+            })
+            link.addEventListener("mouseleave", () => {
+              link.style.transform = "";
+            });
+        })
+    }
+
+
+  return (
+    <div className="social-network">
+      <ul className="content">
+        <a
+          href="https://github.com/otfigo87"
+          target="_blank"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-github"></i>
+          </li>
+        </a>
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-twitter"></i>
+          </li>
+        </a>
+        <a
+          href="https://linkedin.com/in/otmane-aatik"
+          target="_blank"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-linkedin-in"></i>
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialMedia;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import Mouse from '../components/Mouse';
+import SocialMedia from '../components/SocialMedia';
 
 const Home = () => {
   return (
    <div>
     <Mouse/>
     <Navigation />
+    <SocialMedia/>
    </div>
   )
 }
