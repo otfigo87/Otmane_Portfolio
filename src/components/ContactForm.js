2baa63cc6b87e77e8eb0c1db5ff34912bd7575af
Otmane Aatik <aaotmane@gmail.com>
2023-03-29 14:58:41 -0400

    form using EmailJS library

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { init } from "@emailjs/browser";
+init(process.env.ID);
 
 const ContactForm = () => {
-  return (
-    <div>ContactForm</div>
-  )
-}
+  const form = useRef();
 
-export default ContactForm
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const formMess = document.querySelector(".form-message");
+
+    emailjs
+      .sendForm(
+        "service_5ialq58",
+        "template_8ed76yk",
+        form.current,
+        process.env.REACT_APP_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+          formMess.innerHTML = "<p className='success'>Message Sent !</p>";
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+        },
+        (error) => {
+          console.log(error.text);
+          formMess.innerHTML =
+            "<p className='error'>Error! Please try again<p/>";
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+        }
+      );
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Get in touch</h2>
+      <form ref={form} onSubmit={sendEmail} className="form-content">
+        <label>Name</label>
+        <input type="text" name="name" required autoComplete="off" id="name" />
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          required
+          autoComplete="off"
+          id="email"
+        />
+        <label>Message</label>
+        <textarea name="message" required autoComplete="off" id="message" />
+        <input type="submit" value="Send" className="hover button" />
+      </form>
+      <div className="form-message"></div>
+    </div>
+  );
+};
+
+export default ContactForm;
