2239252a36ff0204c8d12e6bc358e250e72c7c6d
Otmane Aatik <aaotmane@gmail.com>
2023-03-30 16:33:13 -0400

    fetch data for the weather icon (requirments)

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Mouse from '../components/Mouse';
 import Navigation from '../components/Navigation';
 import Logo from '../components/Logo';
@@ -6,10 +6,23 @@ import ContactForm from '../components/ContactForm';
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import SocialMedia from "../components/SocialMedia";
 import Buttons from "../components/Buttons";
-import {motion} from "framer-motion"
+import {motion} from "framer-motion";
+import axios from "axios";
 
 const Contact = () => {
 
+  const [weather, setWeather] = useState()
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherapi.com/v1/current.json?key=f8bcee1649274f47a2f180053233003&q=15222&aqi=yes`
+      )
+      .then((res) => setWeather(res.data.current))
+      .catch((err) => console.log(err));
+  }, [weather])
+
+  console.log(weather)
   const pageTransition = {
     in: {
       opacity: 1,
@@ -23,16 +36,36 @@ const Contact = () => {
   return (
     <main>
       <Mouse />
-      <motion.div variants={pageTransition} initial="out" animate="in"  exit="out"
-      transition={{duration: 0.5}} className="contact">
+      <motion.div
+        variants={pageTransition}
+        initial="out"
+        animate="in"
+        exit="out"
+        transition={{ duration: 0.5 }}
+        className="contact"
+      >
         <Navigation />
         <Logo />
         <ContactForm />
         <div className="contact-infos">
           <div className="address">
             <div className="content">
-              <h4>Address</h4>
-              <p>Pittsburgh, PA 15222</p>
+              <p>Pittsburgh</p>
+              <motion.div
+                className="weather"
+                drag
+                dragConstraints={{
+                  left: -250,
+                  right: 250,
+                  top: -200,
+                  bottom: 250,
+                }}
+                style={{ cursor: "pointer" }}
+              >
+                <img src={weather.condition.icon} alt="weather icon" />
+                <h5>{weather.condition.text}</h5>
+                <h4>{`${weather.temp_f} °F`}</h4>
+              </motion.div>
             </div>
           </div>
           <div className="phone">
