e54bc369543c869a41e29e68dd45aaa8f94ad55a
Otmane Aatik <aaotmane@gmail.com>
2023-03-30 11:56:42 -0400

    hme page animation / framer-motion

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import Project3 from "./pages/Project3";
 import Project4 from "./pages/Project4";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/project1" element={<Project1 />} />
-      <Route path="/project2" element={<Project2 />} />
-      <Route path="/project3" element={<Project3 />} />
-      <Route path="/project4" element={<Project4 />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project1" element={<Project1 />} />
+        <Route path="/project2" element={<Project2 />} />
+        <Route path="/project3" element={<Project3 />} />
+        <Route path="/project4" element={<Project4 />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,24 +4,72 @@ import Mouse from "../components/Mouse";
 import SocialMedia from "../components/SocialMedia";
 import DynamicText from "../components/DynamicText";
 import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
 
 const Home = () => {
+  //framer-motion
+  const variants = {
+    initial: {
+      opacity: 0,
+      transition: { duration: 0.5 },
+      x: 100,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+    },
+    exit: {
+      opacity: 0,
+      transition: { duration: 0.3 },
+      x: -100,
+    },
+  };
   return (
     <div>
       <Mouse />
-      <div className="home">
+      <motion.div
+        className="home"
+        initial="initial"
+        animate="visible"
+        exit="exit"
+        variants={variants}
+      >
         <Navigation />
         <SocialMedia />
         <div className="home-main">
           <div className="main-content">
-            <h1>Otmane Aatik</h1>
-            <h2>
+            <motion.h1
+              drag
+              onDragEnd
+              dragConstraints={{
+                left: -250,
+                right: 950,
+                top: -200,
+                bottom: 250,
+              }}
+              style={{ cursor: "pointer" }}
+            >
+              Otmane Aatik
+            </motion.h1>
+            <motion.h2
+              drag
+              onDragEnd
+              dragConstraints={{
+                left: -250,
+                right: 950,
+                top: -200,
+                bottom: 250,
+              }}
+            >
               <DynamicText />
-            </h2>
+            </motion.h2>
           </div>
         </div>
-        <Buttons right={"/project1"} />
-      </div>
+        <Buttons right={"./project1"} />
+        <div className="credits">
+          <p>Web Development</p>
+        </div>
+      </motion.div>
     </div>
   );
 };
